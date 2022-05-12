3b585e79f8dc332b87d6a204a17440b87534d740
Sam Jones <45644425+jones-sam@users.noreply.github.com>
2022-05-12 13:23:47 -0300

    inital setup and form for API key

--- src/stock-lookup.tsx
+++ src/stock-lookup.tsx
@@ -1,9 +1,43 @@
-import React from "react";
-import { ActionPanel, List, Action, Icon } from "@raycast/api";
+import React, { useCallback, useEffect, useState } from "react";
+import { ActionPanel, List, Action, Icon, LocalStorage, Form } from "@raycast/api";
 
-export default function Command() {
+export default function StockLookup() {
+  const [isValidApiKey, setIsValidApiKey] = useState<boolean>(false);
+
+  const getApiKey = useCallback(async () => {
+    const apiKey = await LocalStorage.getItem("apiKey");
+    console.log(apiKey);
+
+    if (apiKey) {
+      setIsValidApiKey(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    getApiKey();
+  }, []);
+
+  if (!isValidApiKey) {
+    return (
+      <Form
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm
+              title="Submit"
+              onSubmit={async (values) => {
+                await LocalStorage.setItem("apiKey", values.apiKey);
+                setIsValidApiKey(true);
+              }}
+            ></Action.SubmitForm>
+          </ActionPanel>
+        }
+      >
+        <Form.TextField id="apiKey" placeholder="API Key" />
+      </Form>
+    );
+  }
   return (
-    <List>
+    <List searchBarPlaceholder="Search for a stock">
       <List.Item title="test" />
       <List.Item title="test2" />
     </List>
